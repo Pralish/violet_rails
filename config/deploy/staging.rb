9404292738b2219a35c11f1b9b68f708f518aaf1
Pralish Kayastha <pralishkayastha@gmail.com>
2022-03-01 18:45:56 +0545

    test parallel deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,6 +8,7 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 server "3.228.40.22", user: 'ubuntu', roles: %w{app db web}
+server "54.163.245.12", user: 'ubuntu', roles: %w{app db web}
 
 # for local precompile
 set :precompile_env, 'staging'
